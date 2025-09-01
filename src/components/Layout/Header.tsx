ea43b5f1c064ca1668d58a8c0824f2afeeb530d2
Meced Loga <121218458+Kingcedloga@users.noreply.github.com>
2025-09-01 10:39:42 +0100

    Updated Header.tsx

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -35,7 +35,7 @@ export const Header: React.FC = () => {
             <img 
               src="/Logo 11.png" 
               alt="Logo" 
-              className="h-50 w-auto"
+              className="h-15 w-auto"
             />
           </Link>
 
