3ba5f54cb87a59be9c2a033dd72662feaabb21ae
Meced Loga <121218458+Kingcedloga@users.noreply.github.com>
2025-09-01 12:32:43 +0100

    Updated Header.tsx

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -57,7 +57,7 @@ export const Header: React.FC = () => {
                 
                 {item.submenu && (
                   <div className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200">
-                    <div className="py-100">
+                    <div className="py-1">
                       {item.submenu.map((subItem) => (
                         <Link
                           key={subItem.name}
