09fff38e5a9945403465220a4b0a3fb0e6c30856
Meced Loga <121218458+Kingcedloga@users.noreply.github.com>
2025-09-01 13:17:34 +0100

    Updated Header.tsx

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -27,8 +27,8 @@ export const Header: React.FC = () => {
   ];
 
   return (
-    <header className="bg-white shadow-xl relative z-30 lg:rounded-full lg:mt-5 px-8 py-6">
-      <div className="max-w-6xl mx-auto">
+    <header className="bg-white shadow-xl relative z-50 lg:rounded-full lg:mt-6 px-8 py-3">
+      <div className="max-w-6xl mx-auto"> 
         <div className="flex justify-between items-center">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2">
