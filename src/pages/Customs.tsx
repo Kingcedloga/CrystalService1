e746fc2e2f5bf02ffb2858db4b920db893f2f97d
Meced Loga <121218458+Kingcedloga@users.noreply.github.com>
2025-09-06 11:07:30 +0100

    Updated Customs.tsx

--- src/pages/Customs.tsx
+++ src/pages/Customs.tsx
@@ -92,7 +92,7 @@ export const Customs: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-50"> 
       {/* Hero Section */}
-      <section className="relative text-white py-16 overflow-hidden fixed top-0 inset-x-0 z-50">
+      <section className="fixed top-0 inset-x-0 z-50 text-white py-16 overflow-hidden">
         {/* Diaporama d'images */}
         <div className="absolute inset-0">
           <motion.img
