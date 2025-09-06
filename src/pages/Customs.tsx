a9d1e5c5446f7e3482c9f35eb50031ba3951c993
Meced Loga <121218458+Kingcedloga@users.noreply.github.com>
2025-09-06 11:48:19 +0100

    Updated Customs.tsx

--- src/pages/Customs.tsx
+++ src/pages/Customs.tsx
@@ -90,7 +90,7 @@ export const Customs: React.FC = () => {
   ];
 
   return (
-    <div className="min-h-screen bg-gray-50"> 
+    <div className="min-h-screen"> 
       {/* Hero Section */}
       <section className="relative text-white py-16 overflow-hidden">
         {/* Diaporama d'images */}
