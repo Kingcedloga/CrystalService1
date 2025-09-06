0d20f5769c0c7a7219c581319f5a5ee0c40ca9ab
Meced Loga <121218458+Kingcedloga@users.noreply.github.com>
2025-09-06 11:44:01 +0100

    Corriger le positionnement de la section Hero dans Customs.tsx

--- src/pages/Customs.tsx
+++ src/pages/Customs.tsx
@@ -92,9 +92,9 @@ export const Customs: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-50"> 
       {/* Hero Section */}
-      <section className="top-0 bottom-10 inset-x-0 text-white py-16 overflow-hidden">
+      <section className="relative text-white py-16 overflow-hidden">
         {/* Diaporama d'images */}
-        <div className="absolute inset-x-0">  
+        <div className="absolute inset-0">
           <motion.img
             key={currentImageIndex}
             src={heroImages[currentImageIndex]}
