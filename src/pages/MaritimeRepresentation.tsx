9a9cf88a91fe39004e63583ab96f9b730e8fd8ed
Meced Loga <121218458+Kingcedloga@users.noreply.github.com>
2025-09-06 16:51:31 +0100

    Updated MaritimeRepresentation.tsx

--- src/pages/MaritimeRepresentation.tsx
+++ src/pages/MaritimeRepresentation.tsx
@@ -269,7 +269,7 @@ export const MaritimeRepresentation: React.FC = () => {
                   Demander un devis
                 </Link>
               </Button>
-              <Button size="lg" variant="outline" className="border-white text-white hover:bg-white hover:text-gray-900" asChild>
+              <Button size="lg" variant="outline" className="border-white text-brand-blue-500 hover:bg-white hover:text-gray-900" asChild>
                 <Link to="/contact">
                   Nous contacter
                 </Link>
