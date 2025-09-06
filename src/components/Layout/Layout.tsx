482b82e61f7dee9eeb5431e43948dc2acf750788
Meced Loga <121218458+Kingcedloga@users.noreply.github.com>
2025-09-06 11:25:11 +0100

    Updated Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col bg-brand-blue-600">
+    <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-1 pt-20 lg:pt-24">
         {children}
